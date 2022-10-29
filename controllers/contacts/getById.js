9e8f94a6426a25c01df2cd933364afc9848e4129
Vitalii Khivrych <khivr.vitali@gmail.com>
2022-10-29 14:25:46 +0300

    add auth middleware in all contact controlles

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -3,7 +3,9 @@ const { RequestError } = require("../../helpers");
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.findById(contactId);
+  const { _id } = req.user;
+
+  const result = await Contact.findOne({ contactId, owner: _id });
 
   if (!result) {
     throw RequestError(404, "Not found");
