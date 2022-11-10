21bcedfe3fcc0e46961058943eac70e47e3fd67b
Vitalii Khivrych <khivr.vitali@gmail.com>
2022-11-10 12:00:21 +0200

    refactoring sendEmail

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -2,12 +2,12 @@ const sgMail = require("@sendgrid/mail");
 
 const { SENDGRID_API_KEY } = process.env;
 
-const sendFromEmail = "khivr.vitali@gmail.com";
+const { SEND_FROM_EMAIL } = process.env;
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: sendFromEmail };
+  const email = { ...data, from: SEND_FROM_EMAIL };
 
   try {
     await sgMail.send(email);
